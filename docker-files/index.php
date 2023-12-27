98a704a51c38bfb69229359ba713dac81a0b8561
Manoranjan Mahakud <mrmahakud95@gmail.com>
2023-12-27 14:21:24 +0530

    index.php modified

--- docker-files/index.php
+++ docker-files/index.php
@@ -1,42 +1,38 @@
-<html>
-    <head>
-        <title>Docker sample app</title>
-        <?php
-        if($_SERVER['REQUEST_METHOD'] == "POST")
-        {
-            $servername = "mysql";
-            $username = "root";
-            $password = "password";
-            $dbname = "mydatabase";
-            $name=$_POST["name"];
-            $phone=$_POST["phone"];
-            //create connection
-            $conn = new mysqli($servername, $username, $password, $dbname);
-            //check connection
-            if(&conn->connect_error){
-                die("connection failed: " . $conn->connect_error);
-
-            }
-            $sql = "INSERT INTO emp (name, phone)
-            VALUES ('".$name."', '".$phone."')";
-            if($conn->query($sql) === TRUE){
-                echo "New record created successfully";
-            
-            }else{
-                echo "Error: " . $sql . "<br>" .$conn->error;
-
-            }
-            $conn->close();
-
-        }
-        ?>
-    </head>
-    <body>
-        <form action="index.php" method="POST">
-            <input type="text" name="name">
-            <input type="text" name="phone">
-            <input type="submit" name="submit">
-    </form>
-    
-    </body>
+<<html>
+<head>
+<title>Docker sample app</title>
+<?php
+if($_SERVER['REQUEST_METHOD'] == "POST")
+{
+        $servername = "172.17.0.4";
+        $username = "root";
+        $password = "password";
+        $dbname = "mydatabase";
+        $name=$_POST["name"];
+        $phone=$_POST["phone"];
+        //create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+//check connection
+if ($conn->connect_error) {
+        die("connection failed: " . $conn->connect_error);
+}
+$sql = "INSERT INTO emp (name, phone)
+        VALUES ('".$name."', '".$phone."')";
+if($conn->query($sql) === TRUE) {
+        echo "New record created successfully";
+}else{
+        echo "Error: ". $sql . "<br>" .$conn->error;
+}
+$conn->close();
+}
+?>
+</head>
+<body>
+ <form action="index.php" method="POST">
+<input type="text" name="name">
+<input type="text" name="phone">
+<input type="submit" name="submit">
+</form>
+</body>
 </html>
+
